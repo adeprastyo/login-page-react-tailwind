0ec7d9458705423620b0802c72e68bafc8ec430a
adeprastyo <adeprastyo101@gmail.com>
2023-05-21 22:28:11 +0700

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <>Hello World</>;
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold ">Hello, Tailwind CSS!</h1>
+      <p className="mt-2">Welcome to my React app with Tailwind CSS.</p>
+    </div>
+  );
 }
 
 export default App;
