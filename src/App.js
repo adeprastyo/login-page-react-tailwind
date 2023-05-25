7042b6532d4888a0057b73ff9140c2c50be5e200
adeprastyo <adeprastyo101@gmail.com>
2023-05-25 12:05:45 +0700

    add background image

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ function App() {
   // ukuran sm (min-width 640px)
   // ukuran lg (min-width 1024px)
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
+    <div
+      className="w-screen h-screen flex justify-center items-center bg-bottom bg-cover "
+      style={{ backgroundImage: "url('./sunrise-urban-house.jpg')" }}
+    >
       <div
-        className="w-4/5 rounded-xl p-8 shadow-xl bg-slate-50
+        className="w-4/5 rounded-3xl p-8 shadow-xl bg-white bg-opacity-10 backdrop-blur-sm
                   sm:w-3/5 sm:p-12
                   lg:w-2/5 lg:p-16"
       >
@@ -23,38 +26,39 @@ function App() {
             LOGIN
           </h1>
           <p
-            className="text-xs text-slate-500 font-light
+            className="text-xs text-white font-light
                         sm:text-base
                         lg:text-lg"
           >
             Don't have an account?{" "}
-            <span className="text-blue-600 font-normal">register</span>
+            <span className="text-blue-300 font-medium">register</span>
           </p>
         </div>
         {/* input */}
         <form>
           <div
-            className="mb-3 text-xs
-                          sm:mb-4 sm:text-base sm:font-medium 
+            className="mb-3 text-xs text-white
+                          sm:mb-4 sm:text-base
                           lg:mb-5 lg:text-lg"
           >
-            <label className="block mb-1 text-slate-500">Email</label>
+            <label className="block mb-1">Email</label>
             <input
-              className="w-full rounded-lg h-9 p-2 text-slate-600 focus:outline-blue-400
+              className="w-full rounded-xl h-9 p-2 text-white focus:outline-white bg-white bg-opacity-40
             sm:px-3 sm:py-5 sm:h-11
             lg:px-4 lg:py-6 lg:h-14"
             />
           </div>
           <div
-            className="mb-3 text-xs
-                          sm:mb-4 sm:text-base sm:font-medium
-                          lg:mb-5 lg:text-lg"
+            className="mb-3 text-xs text-white
+            sm:mb-4 sm:text-base
+            lg:mb-5 lg:text-lg"
           >
-            <label className="block mb-1 text-slate-500">Password</label>
+            <label className="block mb-1">Password</label>
             <input
-              className="w-full rounded-lg h-9 p-2 text-slate-600 focus:outline-blue-400
-            sm:px-3 sm:py-5 sm:h-11
-            lg:px-4 lg:py-6 lg:h-14"
+              type="password"
+              className="w-full rounded-xl h-9 p-2 text-white focus:outline-white bg-white bg-opacity-40
+              sm:px-3 sm:py-5 sm:h-11
+              lg:px-4 lg:py-6 lg:h-14"
             />
           </div>
 
@@ -70,7 +74,7 @@ function App() {
               sm:h-4 sm:w-4
               lg:h-5 lg:w-5"
             />
-            <label for="myCheckbox" class="ml-2 text-slate-500">
+            <label for="myCheckbox" class="ml-2 text-white">
               Save Password
             </label>
           </div>
@@ -81,7 +85,7 @@ function App() {
           lg:mb-4 lg:text-xl"
           >
             <button
-              className="w-full bg-blue-400 text-white py-2 rounded-sm bg-gradient-to-r from-blue-400 to-green-400
+              className="w-full bg-blue-400 text-white py-2 rounded-xl bg-gradient-to-r from-blue-400 to-green-400
             sm:py-3
             lg:py-4"
             >
